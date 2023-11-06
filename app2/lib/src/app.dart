a042a5b8a578c1536cf3b924f9da20b117468dc2
unknown <Billygg2>
2023-11-06 08:56:42 -0500

    si

--- app2/lib/src/app.dart
+++ app2/lib/src/app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  static const String _title="Ejemplo N# 01";
-
+  static const String _title = "Ejemplo N° 01";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,35 +12,69 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyCount extends StatefulWidget{
+class MyCount extends StatefulWidget {
   const MyCount({super.key});
-  
+
   @override
   State<StatefulWidget> createState() => _MyCountState();
 }
 
-class _MyCountState extends State<MyCount>{
+class _MyCountState extends State<MyCount> {
   int _count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      centerTitle: true,
-      title: Text("Ejemplo N° 1",
-      style: TextStyle(fontSize: 35.0, fontFamily: "cursive"),),
-      backgroundColor:  Color.fromARGB(255, 121, 34, 124),
-    ),
-    body: Center(
-      child: Column(
-        children: <Widget> [
-        SizedBox(
-          height: 180.0,
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Ejemplo N° 01",
+            style: TextStyle(fontSize: 35.0, fontFamily: "cursive"),
+          ),
+          backgroundColor: Colors.orange),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 150.0,
+            ),
+            Text("El número es: $_count"),
+            const ClipOval(
+              child: Image(
+                image: AssetImage("imges/images.jpg"),),),
+          ],
         ),
-        Text("El numero es: $_count"),
-          const Image(image: AssetImage("images/images.jpg"))
-        ],
       ),
-    ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 20.0,
+          color: Colors.lightBlueAccent,
+        ),
+      ),
+      floatingActionButton: _crearButtoms(),
+
+    );
+  }
+Widget _crearButtoms() {
+    return Row(
+      children: <Widget>[
+        SizedBox(
+          width: 30.0,
+        ),
+        ElevatedButton(
+          child: Icon(Icons.restore),
+          onPressed: (() {
+            _count = 0;
+          }),
+        ),
+        Expanded(child: SizedBox()),
+        ElevatedButton(
+          child: Icon(Icons.add),
+          onPressed: (() {
+            _count++;
+          }),
+        )
+      ],
     );
   }
 
